6402382d487ff4110f73b7b6e36d244c694e95b6
Andreas Pelme <andreas@pelme.se>
2014-10-02 14:44:18 +0200

    Rename get_query_set to get_queryset

--- idmapper/manager.py
+++ idmapper/manager.py
@@ -3,7 +3,7 @@ from django.db.models.manager import Manager
 from queryset import SharedMemoryQuerySet
 
 class SharedMemoryManager(Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         return SharedMemoryQuerySet(self.model, using=self._db)
 
     use_for_related_fields = True
